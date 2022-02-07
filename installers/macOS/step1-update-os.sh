348758ec8d4383d1b0e21fcbfbb3a09fa2574bdc
David Biersach <dbiersach@gmail.com>
2022-02-06 21:53:26 -0500

    Initial condition

---
+++ installers/macOS/step1-update-os.sh
@@ -0,0 +1,1 @@
+# There are no macOS specific packages to install!
---
+++ installers/macOS/step2-install-tools.sh
@@ -0,0 +1,5 @@
+brew install thonny
+brew install arduino
+brew install visual-studio-code
+brew install miniconda
+brew install powershell
