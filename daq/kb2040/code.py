83e28d5032f8902e0fdecfd13fb5f515ecc6263b
David Biersach <dbiersach@gmail.com>
2022-03-03 02:47:17 -0500

    Fix UART I/O calls in DAQ

--- daq/kb2040/code.py
+++ daq/kb2040/code.py
@@ -11,8 +11,9 @@ import busio
 import time
 import supervisor
 import countio
+import sys
 
-
+# Initialize the on-board (built-in) NeoPixel
 pixel_builtin = neopixel.NeoPixel(board.NEOPIXEL, 1)
 pixel_builtin.brightness = 0.4
 pixel_builtin.fill((0, 0, 0))
@@ -21,17 +22,17 @@ pixel_builtin.fill((0, 0, 0))
 def run_geiger_counter():
     pixel_builtin.fill((0, 255, 0))
     pin_signal = countio.Counter(board.D9, edge=countio.Edge.FALL)
-    time.sleep(3)
-    print(f"{pin_signal.count}")
+    # Count decay events over 30 second period
+    time.sleep(30)
+    sys.stdout.write(f"{pin_signal.count}\n")
     pin_signal.deinit()
     pixel_builtin.fill((0, 0, 0))
 
 
 while True:
     if supervisor.runtime.serial_bytes_available:
-        cmd = input().strip()
-        if cmd == "":
-            continue
-        if cmd == "r":
-            # if cmd == "run_geiger_counter":
+        cmd = sys.stdin.readline().strip()
+
+        if cmd == "run_geiger_counter":
             run_geiger_counter()
+
