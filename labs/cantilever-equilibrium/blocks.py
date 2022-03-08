78afd6fb15fbf7df82f6ca40531b3c7b9791baaf
David Biersach <dbiersach@gmail.com>
2022-03-08 02:27:08 -0500

    Update class hierarchy

--- labs/cantilever-equilibrium/blocks.py
+++ labs/cantilever-equilibrium/blocks.py
@@ -1,31 +1,33 @@
 # blocks.py
 
+
 class Block:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.center_x = x
+        self.center_y = y
 
     def move(self, delta_x, delta_y):
-        self.x = self.x + delta_x
-        self.y = self.y + delta_y
+        self.center_x += delta_x
+        self.center_y += delta_y
 
 
 class BlockList:
     def __init__(self):
         self.blocks = []
         self.num_blocks = 0
-        self.center_x, self.center_y = 0, 0
+        self.center_x = 0
+        self.center_y = 0
 
     def calc_center(self):
-        sum_x, sum_y = 0, 0
+        sum_center_x, sum_center_y = 0, 0
 
         for block in self.blocks:
-            sum_x = sum_x + block.x
-            sum_y = sum_y + block.y
+            sum_center_x += block.center_x
+            sum_center_y += block.center_y
 
         if self.num_blocks > 0:
-            self.center_x = sum_x / self.num_blocks
-            self.center_y = sum_y / self.num_blocks
+            self.center_x = sum_center_x / self.num_blocks
+            self.center_y = sum_center_y / self.num_blocks
 
     def print_center(self):
         self.calc_center()
@@ -35,8 +37,8 @@ class BlockList:
             f"Center Y: {self.center_y:>6.2f}"
         )
 
-    def place_block(self, x, y):
-        self.blocks.append(Block(x, y))
+    def place_block(self, center_x, center_y):
+        self.blocks.append(Block(center_x, center_y))
         self.num_blocks = len(self.blocks)
         self.calc_center()
 
