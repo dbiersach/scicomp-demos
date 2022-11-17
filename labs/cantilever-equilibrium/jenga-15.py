6055126e4bcfcf48285fbf5cbb1f62ddbd5ccfa2
David Biersach <dbiersach@gmail.com>
2022-11-16 23:34:31 -0500

    Switch to Thonny

---
+++ labs/cantilever-equilibrium/jenga-15.py
@@ -0,0 +1,20 @@
+# jenga-15.py
+
+from blocks import BlockList
+
+
+def main():
+    block_list = BlockList()
+
+    block_list.place_block(7.5, 1.5)
+    block_list.place_block(1.5, 10.5)
+    block_list.place_block(10.5, 4.5)
+
+    while block_list.center_x < 15.0:
+        block_list.print_center()
+        block_list.move_blocks(3, 3)
+        block_list.place_block(7.5, 1.5)
+        block_list.place_block(1.5, 10.5)
+
+
+main()
